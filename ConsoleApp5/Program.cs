f4db2da019ad316f1e8c82d0ebb42c1cd521a8da
Opilane TTHK <opilane@tthk.ee>
2023-02-13 14:52:24 +0200

    Initial commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string> { "tea","car","fish","house"};
+            words.ForEach(e=>Console.WriteLine(e));
+        }
+    }
+}
